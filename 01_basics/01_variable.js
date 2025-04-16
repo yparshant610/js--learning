12e63f0c7433c66ae9f3570c59d020f763cbcfa0
Parshant Yadav <77328668+yparshant610@users.noreply.github.com>
2025-04-16 13:38:38 +0000

    1

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId = 123534
+let accountEmail = "parshant@gmail.com"
+var accountPassword = "12345"
+accountCity = "Suratgarh"
+
+
+
+console.log(accountPassword)
+console.table([accountEmail, accountId, accountPassword, accountCity])
